ab7fdb72bdd7f5eb59a52371f433f28be2612801
Adaminite <niazi.omark@gmail.com>
2022-01-05 16:37:33 -0500

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
 
+    for(let i = 1; i < arguments.length; i++){
+        while(arr.indexOf(arguments[i]) !== -1){
+            let idx = arr.indexOf(arguments[i]);
+            arr.splice(idx, 1);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
