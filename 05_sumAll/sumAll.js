5009f8860c70f10616002089f47e24b951444270
Adaminite <niazi.omark@gmail.com>
2022-01-05 16:46:17 -0500

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== "number" || typeof num2 !== "number"|| num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
 
+    let smallest;
+    let largest;
+    
+    if(num1 < num2){
+        smallest = num1;
+        largest = num2;
+    }
+    else{
+        smallest = num2;
+        largest = num1;
+    }
+
+    let sum = 0;
+    for(let ctr = smallest; ctr <= largest; ctr++){
+        sum += ctr;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
