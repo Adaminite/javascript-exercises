583a1ccbc2d147290af2f9baeb45107e6a9777fa
Adaminite <niazi.omark@gmail.com>
2022-01-07 19:53:56 -0500

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    x = Number(x);
+
+    if(typeof x !== "number" || x < 1){
+        return "OOPS";
+    }
+
+
+    let seq = [1,1];
+
+    let i;
+    for(i = 2; i < x; i++){
+        seq.push(seq[i-1] + seq[i-2]);
+    }
+
+    return seq[i-1];
 
 };
 
