a9c1a3aa96cfba42a81352de6e546f26d0354244
Adaminite <niazi.omark@gmail.com>
2022-01-07 20:04:36 -0500

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce( (oldest, curr) =>{
 
+        if(Object.keys(oldest).length === 0){
+            return curr;
+        }
+        let currBirth = curr.yearOfBirth;
+        let oldestBirth = oldest.yearOfBirth;
+
+        let currDeath;
+        let oldestDeath;
+        let currYear = (new Date()).getFullYear();
+        if(curr.yearOfDeath === undefined){
+            currDeath = currYear;
+        }
+        else{
+            currDeath = curr.yearOfDeath;
+        }
+        if(oldest.yearOfDeath === undefined){
+            oldestDeath = currYear;
+        }
+        else{
+            oldestDeath = oldest.yearOfDeath;
+        }
+
+        return ( (currDeath - currBirth) > (oldestDeath - oldestBirth) ) ? curr: oldest;
+    }, {});
 };
 
 // Do not edit below this line
