6c066316de82e8f27634d1f0e18684e299a92dff
Adaminite <niazi.omark@gmail.com>
2022-01-07 19:45:29 -0500

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let modStr = str.replace(/[^a-z]/gi, '').replace(/\s/g, '').toLowerCase();
 
+    let revStrArr = modStr.split('');
+    let revStr = revStrArr.reduce( (string, char) => {
+        string = char + string;
+        return string;
+    }, "");
+
+    if(modStr === revStr){
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
